c2c193b3102b3088ae603d67bbb60c6b906bf28b
jptomas03 <jptomas03@yahoo.com>
2015-08-18 10:48:51 +0800

    exer3
    first commit

---
+++ SAD-EXER3/FlashCard.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FlashCard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.TextBox()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.lblMessage = New System.Windows.Forms.TextBox()
+        Me.cmdNext = New System.Windows.Forms.Button()
+        Me.cmdExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 58.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(207, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 89)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "+"
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 64.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1.Location = New System.Drawing.Point(12, 9)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(189, 97)
+        Me.lblNum1.TabIndex = 1
+        Me.lblNum1.Text = "100"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 64.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum2.Location = New System.Drawing.Point(298, 9)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(189, 97)
+        Me.lblNum2.TabIndex = 2
+        Me.lblNum2.Text = "100"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 58.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(493, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 89)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "="
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 39)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Score"
+        '
+        'lblScore
+        '
+        Me.lblScore.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 58.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(19, 148)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(100, 95)
+        Me.lblScore.TabIndex = 5
+        Me.lblScore.Text = "0"
+        Me.lblScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 58.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAnswer.Location = New System.Drawing.Point(584, 12)
+        Me.txtAnswer.MaxLength = 3
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.Size = New System.Drawing.Size(155, 95)
+        Me.txtAnswer.TabIndex = 6
+        Me.txtAnswer.Text = "0"
+        Me.txtAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblMessage
+        '
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(125, 148)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(453, 47)
+        Me.lblMessage.TabIndex = 7
+        '
+        'cmdNext
+        '
+        Me.cmdNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdNext.Location = New System.Drawing.Point(125, 201)
+        Me.cmdNext.Name = "cmdNext"
+        Me.cmdNext.Size = New System.Drawing.Size(453, 42)
+        Me.cmdNext.TabIndex = 8
+        Me.cmdNext.Text = "Next Problem"
+        Me.cmdNext.UseVisualStyleBackColor = True
+        '
+        'cmdExit
+        '
+        Me.cmdExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdExit.Location = New System.Drawing.Point(584, 131)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(155, 112)
+        Me.cmdExit.TabIndex = 9
+        Me.cmdExit.Text = "EXIT"
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'FlashCard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 261)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Controls.Add(Me.cmdNext)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FlashCard"
+        Me.Text = "Flash Card"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblNum1 As System.Windows.Forms.Label
+    Friend WithEvents lblNum2 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblScore As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents lblMessage As System.Windows.Forms.TextBox
+    Friend WithEvents cmdNext As System.Windows.Forms.Button
+    Friend WithEvents cmdExit As System.Windows.Forms.Button
+
+End Class
---
+++ SAD-EXER3/FlashCard.vb
@@ -0,0 +1,38 @@
+﻿Public Class FlashCard
+    Dim temp As String
+    Private Sub cmdNext_Click(sender As Object, e As EventArgs) Handles cmdNext.Click
+            If txtAnswer.Text = lblNum1.Text + +lblNum2.Text Then
+                lblMessage.Text = "Keep up the good Job :)"
+                lblMessage.BackColor = Color.Green
+                lblScore.Text = 1 + +lblScore.Text
+            Else
+                lblMessage.Text = "Better Luck Next Time"
+                lblMessage.BackColor = Color.Red
+            End If
+        lblNum1.Text = Int(Rnd() * 101)
+        lblNum2.Text = Int(Rnd() * 101)
+
+    End Sub
+
+    Private Sub FlashCard_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lblNum1.Text = Int(Rnd() * 51)
+        lblNum2.Text = Int(Rnd() * 51)
+        temp = txtAnswer.Text
+    End Sub
+
+    Private Sub cmdExit_Click(sender As Object, e As EventArgs) Handles cmdExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtAnswer_TextChanged(sender As Object, e As EventArgs) Handles txtAnswer.TextChanged
+        If txtAnswer.Text = "" Then
+            temp = txtAnswer.Text
+        ElseIf Not Char.IsDigit(txtAnswer.Text.Chars(txtAnswer.TextLength - 1)) Then
+            lblMessage.Text = "Please Input Integer only"
+            lblMessage.BackColor = Color.Yellow
+            txtAnswer.Text = temp
+        Else
+            temp = txtAnswer.Text
+        End If
+    End Sub
+End Class
